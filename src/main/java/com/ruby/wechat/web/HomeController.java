478979e292564f4a339ae0e32ee26d5b1a0eae7d
ruby <liyufeng_23@163.com>
2016-08-12 17:39:09 +0800

    use java config

--- src/main/java/com/ruby/wechat/web/HomeController.java
+++ src/main/java/com/ruby/wechat/web/HomeController.java
@@ -1,7 +1,5 @@
 package com.ruby.wechat.web;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +14,7 @@ public class HomeController {
 
     @RequestMapping(value = {"/","/home","/index"}, method = RequestMethod.GET)
     public String home(ModelMap model) {
-        model.addAttribute("user", getPrincipal());
         return "index";
     }
 
-    private String getPrincipal(){
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails)principal).getUsername();
-        } else {
-            userName = principal.toString();
-        }
-        return userName;
-    }
 }
